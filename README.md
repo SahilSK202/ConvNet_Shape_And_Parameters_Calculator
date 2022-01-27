ecdf6299a30e047f8e52c1df309172a9173ce04a
sahil kavitake <sahir.kavitake202@gmail.com>
2022-01-27 19:21:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ConvNet Shape & Parameters Calculator
+
+Calculates output shape and no of trainable parameters required for convolutional layers based on input shape, stride, kernel size, etc.
+
+## Live Demo: https://SahilSK202.github.io/ConvNet_Shape_And_Parameters_Calculator
+
+<hr>
+
+For more context, see the [CS231n notes](http://cs231n.github.io/convolutional-networks/).<br>
+For parameters calculation [How to calculate the number of parameters in the CNN ?](https://medium.com/@iamvarman/how-to-calculate-the-number-of-parameters-in-the-cnn-5bd55364d7ca)
---
+++ js/main.js
@@ -0,0 +1,83 @@
+function main() {
+  let input_el = document.getElementById("input");
+  let output_el = document.getElementById("output");
+  let message_el = document.getElementById("message");
+  let parameters_el = document.getElementById("n_params");
+  let variable_labels = Array.from(document.querySelectorAll("label"));
+  let variable_sliders = variable_labels.map((x) => x.querySelector("input"));
+  let variable_names = variable_labels.map((x) => x.classList[0]);
+  let v = {};
+  let n_params = 0;
+
+  variable_names.forEach((name, i) => {
+    v[name] = variable_sliders[i];
+  });
+
+  function wrap_em(name, value) {
+    // put numbers as subscript
+    return `<span class="${name}"><em>${value}</em></span>`;
+  }
+
+  function update_input_output() {
+    input_el.innerHTML = `${wrap_em("w1", v.w1.value)}×
+                              ${wrap_em("h1", v.h1.value)}×
+                              ${wrap_em("d1", v.d1.value)}`;
+    var w2 = Math.floor(
+      (v.w1.value - v.f.value + 2 * v.p.value) / v.s.value + 1
+    );
+    var h2 = Math.floor(
+      (v.h1.value - v.f.value + 2 * v.p.value) / v.s.value + 1
+    );
+
+    let message = "";
+    if (w2 < 1 || h2 < 1) {
+      message =
+        "Spatial extent can't be greater than padded input size. Increase P, increase H<sub>1</sub> / W<sub>1</sub>, or reduce F.";
+    } else if (!Number.isInteger(w2) || !Number.isInteger(h2)) {
+    }
+
+    if (!Number.isInteger(w2) || w2 <= 0) {
+      w2 = "❓";
+    }
+    if (!Number.isInteger(h2) || h2 <= 0) {
+      h2 = "❓";
+    }
+
+    output_el.innerHTML = `${wrap_em("w2", w2)}×
+                              ${wrap_em("h2", h2)}×
+                              ${wrap_em("k", v.k.value)}`;
+    message_el.innerHTML = message;
+
+    parameters_el.innerHTML = `Number of Trainable Parameters : <span id="params">${
+      (v.f.value * v.f.value * v.s.value + 1) * v.k.value
+    }</span>`;
+  }
+
+  variable_labels.forEach((label, i) => {
+    let value_el = document.createElement("input");
+    value_el.type = "number";
+    value_el.classList.add("value");
+    label.insertBefore(value_el, variable_sliders[i]);
+    let slider_el = variable_sliders[i];
+    let slider_update = () => {
+      value_el.value = slider_el.value;
+      update_input_output();
+    };
+    let ticker_update = () => {
+      // if ticker exceeds slider max, slider adjusts
+      if (parseInt(value_el.value) > parseInt(slider_el.max)) {
+        slider_el.max = parseInt(value_el.value);
+      }
+      slider_el.value = value_el.value;
+      // if ticker exceeds slider min, ticker adjusts
+      if (parseInt(value_el.value) < slider_el.value) {
+        value_el.value = slider_el.value;
+      }
+      update_input_output();
+    };
+    slider_el.addEventListener("input", slider_update);
+    value_el.addEventListener("input", ticker_update);
+    slider_update();
+  });
+}
+window.onload = main;
